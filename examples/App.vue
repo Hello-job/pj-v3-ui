5778448d887aa5e8c672e6607d5d3ea478ac8577
luchang <835498386@qq.com>
2022-12-04 23:11:44 +0800

    first commit

---
+++ examples/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>启动测试 <Button /></div>
+</template>
+
+<script lang='ts' setup>
+import { Button } from "pj-v3-ui";
+// import "pj-v3-ui/es/style.css";
+</script>
+
+<style>
+</style>
---
+++ packages/components/src/button/button.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pj-button">测试按钮</div>
+</template>
+
+<script>
+import "./style/index.less";
+</script>
+
+<style>
+</style>
